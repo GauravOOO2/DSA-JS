82cf0e94a6d32100162026d21d617ad60f966d9c
Gaurav0002 <gauravking918@gmail.com>
2024-07-14 23:59:48 +0530

    Power of 2

--- pring1To10.js
+++ pring1To10.js
@@ -1,7 +1,17 @@
-var num = 10
+// var num = 10
 
 
-for(var i = 1; i<= num ; i++){
-    console.log(i)
-    console.log("hi")
+// for(var i = 0; i<num ; i++){
+//     console.log(i)
+//     // console.log("hi")
+// }
+
+
+
+function reverseString(str) {
+    // console.log(str)
+    return str.split("").reverse().join("")
+    
 }
+
+console.log(reverseString("hello")); // should return "olleh"
