a9d6679c275f89fc73f13e3e3504a1a1d39375f3
Gaurav0002 <gauravking918@gmail.com>
2024-08-19 14:34:19 +0530

    Bubble sort

--- Binary Search Algo/binarySearchAlgo.js
+++ Binary Search Algo/binarySearchAlgo.js
@@ -1,19 +1,33 @@
-function BinarySearchalgo(arr, target){
+function BinarySearchAlgo(arr, target){
 
-    let LeftIndex = 0;
-    let RightIndex = arr.length - 1;
-    while(LeftIndex<=RightIndex){
-        let MidIndex = Math.floor((LeftIndex + RightIndex) / 2 )
-    if(target===arr[MidIndex]){
-        return MidIndex;
-    }
-    if(target<arr[MidIndex]){
-        RightIndex =  MidIndex -1 
-    }else{
-        LeftIndex = MidIndex + 1
-    }
-}
-return -1;
-}
+let LeftPointer = 0;
+let RightPointer = arr.length - 1
 
-console.log(BinarySearchalgo([-4,-1,1,3,6,8,9,], -4))
+while(LeftPointer<=RightPointer){
+    let MidIndex = Math.floor((LeftPointer + RightPointer)/2)
+    if(target === arr[MidIndex] ) return MidIndex
+
+   
+    target < arr[MidIndex] ? RightPointer = MidIndex - 1 : LeftPointer = MidIndex + 1
+}
+return -1
+}
+console.log(BinarySearchAlgo([-4,-1,1,3,6,8,9,], 8))
+// let LeftPointer = 0;
+// let RightPointer = arr.length - 1;
+
+// while(LeftPointer<=RightPointer){
+//     let MidValue = Math.floor((LeftPointer + RightPointer)/2)
+
+//     if(target === arr[MidValue]){
+//         return MidValue
+//     }
+
+//     if(target < arr[MidValue]){
+//         RightPointer = MidValue - 1
+//     }else{
+//         LeftPointer = MidValue + 1
+//     }
+// }
+// return -1;
+// }
